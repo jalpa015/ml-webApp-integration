49855848ee00ea93e32ff011e4c57db198bff485
Jalpa Patel <pjalpa015@gmail.com>
2022-08-26 00:30:22 -0400

    Added HTML and CSS

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import recommendation
+import sys
 
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/movie', methods=['GET'])
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/movie", methods=["POST"])
 def recommend_movies():
-    res = recommendation.results(request.args.get('title'))
-    return jsonify(res)
+    features = [str(x) for x in request.form.values()]
+    movie_name = str(features[0])
+    print(request, file=sys.stdout)
+    res = recommendation.results(movie_name)
+    print(jsonify(res))
+    return render_template("index.html", recommended_movie=res)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5000, debug=True)
